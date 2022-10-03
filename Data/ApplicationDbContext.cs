eab9fe3988b1c64c70b82ce4e4577cde6034d94a
Franko Azemaj <frankoazemaj@hotmail.com>
2022-10-03 22:37:15 +0200

    Mvc Identity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mvc_IDentity.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
